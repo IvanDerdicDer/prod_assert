f56d0e6469ad4da28a1d0dac7ac07be116853da2
Ivan Derdić <ivanderdic5@gmail.com>
2025-12-13 17:56:01 +0100

    Added source assert

--- src/prod_assert/main.py
+++ src/prod_assert/main.py
@@ -1,1 +1,16 @@
+class AssertionFailed(Exception):
+    pass
+
+
+def prod_assert(condition: bool, message: str) -> None:
+    if not isinstance(condition, bool):
+        raise ValueError("Paramater condition is not bool")
+
+    if not isinstance(message, str):
+        raise ValueError("Paramater message is not bool")
+
+    if condition:
+        return
+
+    raise AssertionFailed(message)
 
