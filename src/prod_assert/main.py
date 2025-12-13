392ed19f582109ca6381492ea3d5b9bf17d7418d
Ivan Derdić <ivanderdic5@gmail.com>
2025-12-13 18:03:49 +0100

    Added source assert

--- src/prod_assert/main.py
+++ src/prod_assert/main.py
@@ -3,6 +3,18 @@ class AssertionFailed(Exception):
 
 
 def prod_assert(condition: bool, message: str) -> None:
+    """
+    Function that takes a condition and checks if it is True.
+    If the condition is False the function raises AssertionFailed exception.
+
+    :param condition: Condition to check.
+    :type condition: bool
+    :param message: Message to display if check fails.
+    :type message: str
+    :raises ValueError: If condition is not bool.
+    :raises ValueError: If message is not str.
+    :raises AssertionFailde: If condition is False
+    """
     if not isinstance(condition, bool):
         raise ValueError("Paramater condition is not bool")
 
